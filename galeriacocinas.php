034eb465d644cffbc016fc655e24b79b73154c9b
Gustavo Adolfo Muñoz Reyes <Gustavo Adolfo Muoz Reyes>
2016-05-26 20:41:05 -0500

    inicio de galeriacocina

---
+++ galeriacocinas.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Galeria Cocina</title>
+	<link rel="stylesheet" href="css/ed-grid.css">
+	<link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/lib/bootstrap.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body class="fondo_cocina">
+	<div class="menuFixed-Main">
+		<div class="menuFixed">
+			<div class="container">
+				<div class="row">
+					<nav class="col-sm-12 col-md-12 navbar navbar-default navbar-static-top">
+						<div class="collapse navbar-collapse menu" id="bs-example-navbar-collapse-1">
+							<ul class="nav navbar-nav">
+								<li><a href="/">Inicio</a></li>
+								<li><a href="/">¿Cómo participar?</a></li>
+								<li><a href="cocina.php">CREA TU COCINA</a></li>
+								<li><a href="/#productos">Nuestros <br> productos</a></li>
+								<li><a href="/#premio">Premio</a></li>
+								<li><a href="/" id="termino" class="mTop">Términos y <br>condiciones</a></li>
+								<li><a href="/#contacto" id="contacto" class="line-height">Contacto</a></li>
+							</ul>
+						</div>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</div>
+		<h1 class="total text-center color-fuente">Pinta tu cocina</h1>
+		<br>
+	<div class="ed-container">
+		<div class="ed-item base-100 movil-100 tablet-50 web-50">
+			<img src="images/1.jpg" alt="">
+		</div>
+		<div class="ed-item base-100 movil-100 tablet-50 web-50">
+			<img src="images/2.jpg" alt="">
+		</div>
+	</div>	
+</body>
+</html>
